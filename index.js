d18a80323e60b5c4f360ad2495f48f1e8c3a808d
diellas8 <58296399+diellas8@users.noreply.github.com>
2023-08-16 17:23:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-export * from './Create'
+import styles from "./header.module.css";
+
+
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1>Your Notes</h1>
+      <button className={styles.closeButton} onClick={() => console.log("1")}>X</button>
+    </div>
+  );
+};
+
